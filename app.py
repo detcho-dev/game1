e48197447ee048a01e676514d76f3fefdb6d28b8
detcho-dev <detcho476044028@gmail.com>
2025-05-02 16:55:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
+from flask import Flask, render_template, request, redirect, session, url_for
+import json, os
 
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+
+@app.route('/')
+def home():
+    return redirect(url_for('game'))
+
+@app.route('/game', methods=['GET', 'POST'])
+def game():
+    if request.method == 'POST':
+        answer = request.form.get('answer', '').lower()
+        session['response'] = f"You said: {answer}"
+        # Here you'll handle the logic of the game based on the answer
+        return redirect(url_for('game'))
+
+    message = session.get('message', 'Welcome to the Investor Game! Start playing.')
+    response = session.get('response', '')
+    return render_template('game.html', message=message, response=response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
