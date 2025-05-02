ac57e56ad4c7ef0b5301cdc2c168516cf3e1812e
detcho-dev <detcho476044028@gmail.com>
2025-05-02 16:26:40 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,7 +5,3 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Welcome to Investor Game!"
-
-# ده الجزء المهم عشان Vercel يقدر يقرأ الـ app
-def handler(request, response):
-    return app(request, response)
